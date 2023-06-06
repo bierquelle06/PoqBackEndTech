052820dbc0be4c7a64a3c9a8417025eebdf30a14
Aykut Aktaş <aaktas@kazandirio-dev.com>
2023-06-06 12:24:17 +0300

    .

--- SampleProject.API/Products/ProductsController.cs
+++ SampleProject.API/Products/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Drawing;
+using System.Net;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -20,12 +21,42 @@ namespace SampleProject.API.Customers
         }
 
         /// <summary>
+        /// Filter with parameter 
+        /// URL : filter?minprice=5&maxprice=20&size=medium&highlight=green,blue
+        /// </summary>
+        /// <param name="minprice"></param>
+        /// <param name="maxprice"></param>
+        /// <param name="size"></param>
+        /// <param name="highlight"></param>
+        /// <returns></returns>
+        [Route("filter")]
+        [HttpGet]
+        [ProducesResponseType(typeof(ProductFilterDto), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Filter(decimal? minprice, decimal? maxprice, string size, string highlight)
+        {
+            var request = new FilterProductRequest() {
+                Size = size,
+                MinPrice = minprice,
+                MaxPrice = maxprice,
+                Highlight = highlight
+            };
+
+            var result = await _mediator.Send(new FilterProductCommand(
+                minPrice: request.MinPrice,
+                maxPrice: request.MaxPrice,
+                size: request.Size,
+                highlight: request.Highlight));
+
+            return Ok(result);
+        }
+
+        /// <summary>
         /// Filter product
         /// </summary>
-        [Route("filter")]
+        [Route("filterproduct")]
         [HttpPost]
         [ProducesResponseType(typeof(ProductFilterDto), (int)HttpStatusCode.OK)]
-        public async Task<IActionResult> RegisterCustomer([FromBody] FilterProductRequest request)
+        public async Task<IActionResult> FilterProduct([FromBody] FilterProductRequest request)
         {
             var result = await _mediator.Send(new FilterProductCommand(
                 minPrice: request.MinPrice,
